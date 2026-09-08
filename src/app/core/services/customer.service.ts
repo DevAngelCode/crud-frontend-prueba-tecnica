83f1a13d5f4c2c1d9df571cb27b95dfdb7608a70
dev <240357148+DevAngelCode@users.noreply.github.com>
2026-09-08 16:56:48 -0500

    creacion de services para el crud y config

---
+++ src/app/core/services/customer.service.ts
@@ -0,0 +1,48 @@
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CustomerModel } from '../models/customer.model';
+import { MessageResponseModel } from '../models/message-response.model';
+import { CustomerCreateRequestModel } from '../models/customer-create-request.model';
+import { CustomerUpdateRequestModel } from '../models/customer-update-request.model';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+  private readonly http = inject(HttpClient);
+
+  private readonly apiUrl = 'http://localhost:8080/api/v1/customers';
+
+  findAll(): Observable<CustomerModel[]> {
+    return this.http.get<CustomerModel[]>(this.apiUrl);
+  }
+
+  findById(id: number): Observable<CustomerModel> {
+    return this.http.get<CustomerModel>(`${this.apiUrl}/${id}`);
+  }
+
+  create(request: CustomerCreateRequestModel): Observable<MessageResponseModel> {
+    return this.http.post<MessageResponseModel>(
+      this.apiUrl,
+      request
+    );
+  }
+
+  update(
+    id: number,
+    request: CustomerUpdateRequestModel
+  ): Observable<MessageResponseModel> {
+    return this.http.put<MessageResponseModel>(
+      `${this.apiUrl}/${id}`,
+      request
+    );
+  }
+
+  delete(id: number): Observable<MessageResponseModel> {
+    return this.http.delete<MessageResponseModel>(
+      `${this.apiUrl}/${id}`
+    );
+  }
+  constructor() { }
+}
